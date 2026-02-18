4d902e6987702be6eaaa3ed5e0cc69ca218a44c7
Sivaharsha <sivaharsha.sottu@gmail.com>
2026-02-18 11:30:37 +0530

    edit 1

--- ps1b.py
+++ ps1b.py
@@ -1,17 +1,20 @@
 while True:
     # asks user until the user enters correct input.
-    
+
     try:
 
         Cost_of_house = int(input("enter the total cost of dream home :"))
         # user enters his dream house cost.
-        if Cost_of_house > 0:     
+        if Cost_of_house > 0:
             # checks user entered cost of house.
             Down_payment = 0.25 * Cost_of_house
             # calculates downpayment of your dream house.
             savings = 0.0
             break
-        
+        else:
+            print("Cost of house must be greater then 0.")
+            continue
+
     except ValueError:
 
         print("invalid input!.please input correct cost.")
@@ -19,7 +22,7 @@ while True:
         # checks user entered correct value or not
 while True:
     # asks user until the user enters correct input.
-    
+
     try:
 
         annual_salary = float(input("Enter your annual salary :"))
@@ -39,10 +42,11 @@ while True:
 
 while True:
     # asks user until the user enters correct input.
-    
+
     try:
-        
-        Portion_saved = float(input("Enter the percent of your annual salary to save, as a decimal (> 0 and < 1) : "))
+
+        Portion_saved = float(input(
+            "Enter the percent of your annual salary to save, as a decimal (> 0 and < 1) : "))
 
         if 0 < Portion_saved <= 1:
             break
@@ -50,11 +54,11 @@ while True:
             print("please enter in decimal value (> 0 and < 1)")
     except ValueError:
         print("please enter correct portion saved annualy in percantage (0-1) :")
-            # checks user entered correct portion saved or not.
+        # checks user entered correct portion saved or not.
 
 while True:
     # asks user until the user enters correct input.
-    
+
     try:
 
         Annual_return = int(input("Enter annual return on investment :"))
@@ -72,9 +76,10 @@ while True:
         # checks user entered correct annual return.
 while True:
     # asks user until the user enters correct input.
-    
+
     try:
-        semi_annual_rise = float(input("enter semi annual rise, in decimal (> 0 and <= 1) :"))
+        semi_annual_rise = float(
+            input("enter semi annual rise, in decimal (> 0 and <= 1) :"))
         # user enters semi annual rise.
         if semi_annual_rise > 0 and semi_annual_rise <= 1:
             # checks input is between 0 and 1.
@@ -85,20 +90,20 @@ while True:
     except ValueError:
         print("please enter correct decimal value.")
         # prints appropriate message when value is incorrect.
-        
+
 months = 0
 while savings < Down_payment:
     # checks condition until savings less than required down payment.
-    savings +=  savings * monthly_return
-    savings += monthly_salary *  Portion_saved
-    months+=1
+    savings += savings * monthly_return
+    savings += monthly_salary * Portion_saved
+    months += 1
     # add one month for each iteration.
-    
+
     if months % 6 == 0:
         # checks month is divisible by 6 or 6 months is completed or not.
         annual_salary += annual_salary * semi_annual_rise
         # adds semi annual rise to annual salary.
         monthly_salary = annual_salary / 12
         # converts annual salary in to monthly salary.
-        
+
 print(f"no of months = {months}")     # prints no of months.
